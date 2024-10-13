4f7ce376fdbb2d5e0ae0c5fb2b9ce1cab0054503
SamDOU2085 <dou.samir@yahoo.fr>
2024-10-13 22:50:24 +0200

    backend

--- app.py
+++ app.py
@@ -1,87 +1,90 @@
-from flask import Flask, request, jsonify, send_from_directory
+import streamlit as st
 import pandas as pd
-import numpy as np
-import joblib
-import os
-import pickle
-from functions.functions import load_data, extract_features_from_custom, predict_score, generate_shap_image, generate_feature_distributions
+import requests
+import plotly.graph_objs as go
+import plotly.io as pio
+import json
+import time
+from streamlit_option_menu import option_menu
 
-# Charger les données
-df, customer_ids = load_data()
+# Charger les données clients
+data_path = 'data/customers.csv'
+df = pd.read_csv(data_path)
+customer_ids = df['SK_ID_CURR'].astype(str).tolist()
 
-app = Flask(__name__)
+# URL de base pour l'API déployée sur Heroku
+base_url = "https://backendflaskforscoring-12eba9fb5ac8.herokuapp.com"
 
-@app.route('/')
-def welcome():
-    # Retourne la liste des IDs clients en JSON pour potentiellement l'utiliser côté client
-    return jsonify({"customer_ids": customer_ids})
+# Configuration de la barre latérale avec une liste déroulante pour sélectionner l'ID du client
+with st.sidebar:
+    st.write("### Sélectionnez un Client ID")
+    selected_customer_id = st.selectbox("Client ID", customer_ids)
+    num_features = st.slider("Nombre de features SHAP à afficher", min_value=1, max_value=20, value=5)
+    decision_threshold = st.slider("Seuil de décision", min_value=0.0, max_value=1.0, value=0.25, step=0.05)
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Récupérer l'ID du client à partir de la requête JSON envoyée par Streamlit
-    selected_id = int(request.json.get('customer_id'))
+# Navigation entre les trois sous-panneaux
+selected_panel = option_menu(
+    menu_title=None,
+    options=["Résultat Prêt", "Graphique SHAP", "Distributions"],
+    icons=["check-circle", "chart-bar", "chart-line"],
+    menu_icon="cast",
+    default_index=0,
+    orientation="horizontal",
+)
+
+# Vérification de la sélection et récupération des données de prédiction
+if selected_customer_id:
+    response = requests.post(f"{base_url}/predict", json={"customer_id": selected_customer_id})
     
-    # Extraire les caractéristiques du client
-    customer_data = extract_features_from_custom(df, selected_id)
-    
-    # Réaliser la prédiction
-    decision, prediction_success, prediction_failure = predict_score(customer_data)
-    
-    # Retourne les résultats de la prédiction sous forme de JSON
-    return jsonify({
-        "decision": decision,
-        "prediction_success": prediction_success,
-        "prediction_failure": prediction_failure,
-        "customer_id": selected_id
-    })
-
-@app.route('/result', methods=['GET'])
-def show_prediction():
-    # Récupère les informations de la requête pour les renvoyer en JSON
-    decision = request.args.get('decision')
-    prediction_success = request.args.get('prediction_success')
-    prediction_failure = request.args.get('prediction_failure')
-    customer_id = request.args.get('customer_id')
-    
-    # Retourner les informations du résultat sous forme de JSON pour usage client
-    return jsonify({
-        "decision": decision,
-        "prediction_success": prediction_success,
-        "prediction_failure": prediction_failure,
-        "customer_id": customer_id
-    })
-
-@app.route('/explain/<int:customer_id>', methods=['GET'])
-def explain(customer_id):
-    try:
-        customer_data_raw = extract_features_from_custom(df, customer_id)
-        plot_path = generate_shap_image(customer_data_raw)
+    if response.ok:
+        data = response.json()
+        prediction_success = data.get("prediction_success")
+        prediction_failure = data.get("prediction_failure")
+        decision = data.get("decision")
         
-        # Retourner le chemin complet de l'image
-        return jsonify({"image_url": f"{request.host_url}static/images/{plot_path}"})
-    except Exception as e:
-        print("Erreur dans la route explain:", str(e))
-        return jsonify({"error": "Une erreur s'est produite lors de la génération de l'explication."}), 500
+        # Page 1 : Jauge et Résultat de Prêt
+        if selected_panel == "Résultat Prêt":
+            st.write("## Résultat de l'accord de prêt")
+            decision_finale = "Accordé" if prediction_failure <= decision_threshold else "Refusé"
+            st.write(f"**Décision finale à un seuil de {decision_threshold}:** {decision_finale}")
+            gauge = go.Figure(go.Indicator(
+                mode="gauge+number",
+                value=prediction_failure,
+                title={"text": "Probabilité de défaut de paiement"},
+                gauge={'axis': {'range': [0, 1]},
+                       'steps': [
+                           {'range': [0, decision_threshold], 'color': "lightgreen"},
+                           {'range': [decision_threshold, 1], 'color': "red"}]},
+            ))
+            st.plotly_chart(gauge)
 
-# Route pour servir l'image
-@app.route('/static/images/<path:filename>')
-def serve_image(filename):
-    return send_from_directory("static/images", filename)
+        # Page 2 : Graphique SHAP (adapté pour base64)
+        elif selected_panel == "Graphique SHAP":
+            st.write("## Explication du modèle : SHAP")
+            shap_response = requests.get(f"{base_url}/explain/{selected_customer_id}")
+            if shap_response.ok:
+                shap_data = shap_response.json()
+                image_base64 = shap_data.get("image_base64")
+                if image_base64:
+                    # Générer l'élément HTML pour afficher l'image en base64
+                    image_html = f"<img src='data:image/png;base64,{image_base64}'/>"
+                    st.markdown(image_html, unsafe_allow_html=True)
+                else:
+                    st.error("Erreur: L'image SHAP n'a pas pu être chargée.")
+            else:
+                st.error("Erreur lors de la récupération du graphique SHAP.")
 
-@app.route('/distributions/<int:customer_id>', methods=['GET'])
-def distributions(customer_id):
-    import json
-    import plotly
-    try:
-        fig = generate_feature_distributions(df, customer_id)
-        fig_json = json.loads(plotly.io.to_json(fig))
-        return jsonify(fig_json)
-    except Exception as e:
-        print("Erreur dans la génération des distributions:", str(e))
-        return jsonify({"error": "Une erreur s'est produite"}), 500
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
+        # Page 3 : Distributions des Features
+        elif selected_panel == "Distributions":
+            st.write("## Positionnement du client par rapport aux autres clients")
+            dist_response = requests.get(f"{base_url}/distributions/{selected_customer_id}")
+            if dist_response.ok:
+                fig_data = dist_response.json()
+                fig = pio.from_json(json.dumps(fig_data))
+                st.plotly_chart(fig)
+            else:
+                st.error("Erreur lors de la récupération des distributions des features.")
+    else:
+        st.error("Erreur lors de la récupération des données de prédiction. Veuillez vérifier l'ID client et réessayer.")
+else:
+    st.info("Veuillez sélectionner un Client ID dans la barre latérale pour commencer.")
