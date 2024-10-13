2606c1cbb7f910025ed50f2170657fdcca09bbbb
SamDOU2085 <dou.samir@yahoo.fr>
2024-10-13 23:13:26 +0200

    backend

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory, Response
 import pandas as pd
 import numpy as np
 import joblib
